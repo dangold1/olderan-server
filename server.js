42d386438a996ca6cd0c4b4fe96f2134770953b9
dangold1 <dangold308@gmail.com>
2021-01-28 18:49:28 +0200

    update env

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const { MONGODB_URI, PORT = 8080 } = process.env;
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
